ee2e056443a7e6b86ce641e5bbab7fab6d690fe9
Jung Hwan <choijh5779@gmail.com>
2025-02-07 23:06:20 +0900

    Update 최소， 최대.c

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.c
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
- 
-int main(void){
-    int N;
-    scanf("%d", &N);
-    int arr[N];
-    
-    for(int i=0; i<N; i++){
-        scanf("%d", &arr[i]);
+
+int main(void) {
+    int x;
+    scanf("%d", &x);
+    int y[x];
+    int max, min;
+
+    for(int i=0; i<x; i++) {
+        scanf("%d", &y[i]);
     }
- 
-    int min = arr[0], max = arr[0];
-    for(int j=0; j<N; j++){
-        if(arr[j] < min)
-            min = arr[j];
-        if(arr[j] > max)
-            max = arr[j];
+
+    max = y[0];
+    min = y[0];
+
+    for(int i=0; i<x; i++) {
+        if(y[i] < min) min = y[i];
+        if(y[i] > max) max = y[i];
     }
- 
+
     printf("%d %d", min, max);
 }
